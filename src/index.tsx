a60186f47e1789662f8775c206d0a50db87ee751
Nick <nikmaunt@gmail.com>
2023-01-08 21:18:16 +0300

    adding Hash Router

--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,18 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import {Provider} from "react-redux";
 import {store} from "./features/store";
+import {HashRouter} from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+      <HashRouter>
       <Provider store={store}>
           <App />
       </Provider>
+      </HashRouter>
   </React.StrictMode>
 );
 
